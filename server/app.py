95dc43c2fedbab8468d2e0786c693414a0627e30
Joy Omonya <joynandako1@gmail.com>
2024-07-01 05:03:45 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,26 +1,42 @@
-# server/app.py
-#!/usr/bin/env python3
+# app.py
 
-from flask import Flask, make_response
-from flask_migrate import Migrate
-
-from models import db, Earthquake
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+db = SQLAlchemy(app)
 
-migrate = Migrate(app, db)
-db.init_app(app)
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    
+    serialize_only = ('id', 'magnitude', 'location', 'year')
 
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
 
-@app.route('/')
-def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify(earthquake.to_dict())
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
 
-# Add views here
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    return jsonify({
+        "count": len(earthquakes),
+        "quakes": [earthquake.to_dict() for earthquake in earthquakes]
+    })
 
 
 if __name__ == '__main__':
